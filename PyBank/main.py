b22c5274729cd15c869ddbe44bba7a6e5b2f2b9f
will-stephens6 <williamhstephens.2018@gmail.com>
2023-10-28 08:56:02 -0600

    final version

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,23 +1,26 @@
 import csv
 
+# Define a function to calculate the average change in profit/loss.
 def calculate_average_change(monthly_pL):
     total_change = 0
-    for x in range(1,len(monthly_pL)):#loop from 1 to the end 
+    for x in range(1,len(monthly_pL)):#loop through starting at second row 
         total_change += int(monthly_pL[x][1])-int(monthly_pL[x-1][1]) #formula to find the monthly change 
-    return total_change/(len(monthly_pL)-1) #return the value 
+    return total_change/(len(monthly_pL)-1) #return the avg change  
 
+# Define a function to find the month with the greatest increase in profit.
 def find_greatest_change(monthly_pL):
     greatest_change = ["",0]
-    for x in range(1,len(monthly_pL)):#loop from 1 to the end 
+    for x in range(1,len(monthly_pL)):#loop through starting at second row  
         current_change = int(monthly_pL[x][1])-int(monthly_pL[x-1][1]) #formula to find the monthly change 
-        if current_change > greatest_change[1]:
+        if current_change > greatest_change[1]: #check if current change is greater than current greatest change
             greatest_change[1] = current_change
             greatest_change[0] = monthly_pL[x][0]
-    return greatest_change 
+    return greatest_change #return the greatest change month
 
+# Define a function to find the month with the greatest decrease in profit.
 def find_smallest_change(monthly_pL):
     smallest_change = ["",0]
-    for x in range(1,len(monthly_pL)):#loop from 1 to the end 
+    for x in range(1,len(monthly_pL)):#loop through starting at second row  
         current_change = int(monthly_pL[x][1])-int(monthly_pL[x-1][1]) #formula to find the monthly change 
         if current_change < smallest_change[1]:
             smallest_change[1] = current_change
@@ -36,10 +39,10 @@ with open("PyBank/Resources/budget_data.csv", mode="r") as file: #open the csv
         total_profit_loss = total_profit_loss + int(line[1])#add each month's profit and losses together for each line
         budget_data_list.append(line)#add each month's profit/loss to the budget list 
 
-    total_months = len(budget_data_list)  
-    average_change = calculate_average_change(budget_data_list)
-    greatest_change = find_greatest_change(budget_data_list)
-    smallest_change = find_smallest_change(budget_data_list)
+    total_months = len(budget_data_list) #calculate total number of months 
+    average_change = calculate_average_change(budget_data_list) #calculate average 
+    greatest_change = find_greatest_change(budget_data_list) #calculate greatest increase 
+    smallest_change = find_smallest_change(budget_data_list) #calculate greatest decrease
 
     #print each line 
     print("Financial Analysis") 
@@ -49,3 +52,13 @@ with open("PyBank/Resources/budget_data.csv", mode="r") as file: #open the csv
     print(f"Average Change: ${round(average_change,2)}")
     print(f"Greatest Change: {greatest_change[0]} (${round(greatest_change[1],2)})")
     print(f"Smallest Change: {smallest_change[0]} (${round(smallest_change[1],2)})")
+
+ # Write to a .txt file 
+    with open("PyBank/analysis/financial_analysis.txt", "w") as result_file:
+        result_file.write("Financial Analysis\n")
+        result_file.write("----------------------------\n")
+        result_file.write(f"Total Months: {total_months}\n")
+        result_file.write(f"Total: ${total_profit_loss}\n")
+        result_file.write(f"Average Change: ${round(average_change, 2)}\n")
+        result_file.write(f"Greatest Increase in Profits: {greatest_change[0]} (${greatest_change[1]})\n")
+        result_file.write(f"Greatest Decrease in Profits: {smallest_change[0]} (${smallest_change[1]})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,24 +7,44 @@ with open("PyPoll/Resources/election_data.csv", mode="r") as file: #open the csv
     total_votes = 0
     candidates_votes = {}
     
-    for ballot in csv_reader:
-        total_votes += 1 
-        if ballot[2] in candidates_votes:
-            candidates_votes[ballot[2]] += 1
+    for ballot in csv_reader:#look through the csv 
+        total_votes += 1 #finds total of rows which is total votes 
+        if ballot[2] in candidates_votes: #conditional to count 2nd index 
+            candidates_votes[ballot[2]] += 1 
         else:
             candidates_votes[ballot[2]] = 1
 
-    for candidate,votes in candidates_votes.items():
-        print("Election Results")
-        print(f"{candidate} %{(votes/total_votes)*100} {votes}")
-    
-    winner = next(iter(candidates_votes))
-    for candidate,votes in candidates_votes.items():
-        if candidates_votes[winner] < candidates_votes[candidate]:
-            winner = candidate
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
 
+
+    for candidate, votes in candidates_votes.items():
+        vote_percentage = (votes / total_votes) * 100
+        print(f"{candidate}: {vote_percentage:.3f}% ({votes})")
+
+    
+    winner = max(candidates_votes, key=candidates_votes.get)
     print(f"Winner: {winner}")
-        
+    print("-------------------------")
+
+#write file to txt
+with open("PyPoll/analysis/election_analysis.txt", "w") as result_file:
+    result_file.write("Election Results\n")
+    result_file.write("-------------------------\n")
+    result_file.write(f"Total Votes: {total_votes}\n")
+    result_file.write("-------------------------\n")
+
+    for candidate, votes in candidates_votes.items():
+        vote_percentage = (votes / total_votes) * 100
+        result_file.write(f"{candidate}: {vote_percentage:.3f}% ({votes})\n")
+
+    result_file.write("-------------------------\n")
+    result_file.write(f"Winner: {winner}\n")
+    result_file.write("-------------------------\n")
+
+
 
 
         
